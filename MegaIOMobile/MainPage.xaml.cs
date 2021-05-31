6d702f2c91770860da26c93742a8aa70c5b9122b
zohair <zohair@maavan.com>
2021-06-01 02:53:31 +0500

    Login & File Structure

--- MegaIOMobile/MainPage.xaml.cs
+++ MegaIOMobile/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CG.Web.MegaApiClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,6 +14,56 @@ namespace MegaIOMobile
         public MainPage()
         {
             InitializeComponent();
+            Task.Run(Main);
+        }
+        void Main()
+        {
+            var client = new MegaApiClient();
+            client.Login("zap_pirzada@hotmail.com", "***********");
+
+            // GetNodes retrieves all files/folders metadata from Mega
+            // so this method can be time consuming
+            IEnumerable<INode> nodes = client.GetNodes();
+
+            INode parent = nodes.Single(n => n.Type == NodeType.Root);
+            DisplayNodesRecursive(nodes, parent);
+
+            client.Logout();
+        }
+
+        void DisplayNodesRecursive(IEnumerable<INode> nodes, INode parent, int level = 0)
+        {
+            IEnumerable<INode> children = nodes.Where(x => x.ParentId == parent.Id);
+            foreach (INode child in children)
+            {
+                string infos = $"- {child.Name} - {FileSizeFormatter.FormatSize(child.Size)} - {child.CreationDate}";
+                Console.WriteLine(infos.PadLeft(infos.Length + level, '\t'));
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    M.Text += infos.PadLeft(infos.Length + level, '\t') + Environment.NewLine;
+                });
+                if (child.Type == NodeType.Directory)
+                {
+                    DisplayNodesRecursive(nodes, child, level + 1);
+                }
+            }
+        }
+        public static class FileSizeFormatter
+        {
+            // Load all suffixes in an array  
+            static readonly string[] suffixes =
+            { "Bytes", "KB", "MB", "GB", "TB", "PB" };
+            public static string FormatSize(Int64 bytes)
+            {
+                int counter = 0;
+                decimal number = (decimal)bytes;
+                while (Math.Round(number / 1024) >= 1)
+                {
+                    number = number / 1024;
+                    counter++;
+                }
+                return string.Format("{0:n1}{1}", number, suffixes[counter]);
+            }
         }
     }
 }
